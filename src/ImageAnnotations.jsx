12ff852ec3d5a5753f9ae66a55ce3147d88efc10
antonnprince <antonprince95@gmail.com>
2024-03-08 21:13:39 +0530

    file upload

--- src/ImageAnnotations.jsx
+++ src/ImageAnnotations.jsx
@@ -4,9 +4,22 @@ import IMAGE_URL from './h2.png'
 import './App.css';
 
 const ImageAnnotations = () => {
-    
+  const [image, setImage] = useState(null);
     const [annotations, setAnnotations] = useState([]);
     const [annotation, setAnnotation] = useState({});
+
+    const handleImageUpload = (event) => {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+  
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
+  
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    };
 
   const onChange = (annotation) => {
     setAnnotation(annotation);
@@ -29,10 +42,11 @@ const ImageAnnotations = () => {
   return (
     <div className="App">
       <header className="App-header">
-       
+      <h1>Image Annotation</h1>
+        <input type="file" accept="image/*" onChange={handleImageUpload} />
         <Annotation
-          src={IMAGE_URL}
-          alt='Two pebbles anthropomorphized holding hands'
+          src={image}
+          // alt='Two pebbles anthropomorphized holding hands'
           annotations={annotations}
           value={annotation}
           onChange={onChange}
