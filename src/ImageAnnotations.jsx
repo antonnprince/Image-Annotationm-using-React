afbeb986c4485dd250dd1f27065106f58fdc9da9
antonnprince <antonprince95@gmail.com>
2024-03-08 22:10:05 +0530

    display x and y values

--- src/ImageAnnotations.jsx
+++ src/ImageAnnotations.jsx
@@ -7,6 +7,10 @@ const ImageAnnotations = () => {
   const [image, setImage] = useState(null);
     const [annotations, setAnnotations] = useState([]);
     const [annotation, setAnnotation] = useState({});
+    const [X,setX] = useState(0)
+    const [Y, setY] = useState(0)
+    const[H,setH] = useState(0)
+    const[W,setW] = useState(0)
 
     const handleImageUpload = (event) => {
       const file = event.target.files[0];
@@ -27,6 +31,12 @@ const ImageAnnotations = () => {
 
   const onSubmit = (annotation) => {
     const { geometry, data } = annotation;
+    console.log(geometry);
+    console.log(data)
+    setX(geometry.x)
+    setY(geometry.y)
+    setH(geometry.height)
+    setW(geometry.width)
 
     setAnnotation({});
     setAnnotations([...annotations, {
@@ -44,15 +54,30 @@ const ImageAnnotations = () => {
       <header className="App-header">
       <h1>Image Annotation</h1>
         <input type="file" accept="image/*" onChange={handleImageUpload} />
-        <Annotation
-          src={image}
-          // alt='Two pebbles anthropomorphized holding hands'
-          annotations={annotations}
-          value={annotation}
-          onChange={onChange}
-          onSubmit={onSubmit}
-          allowTouch
-        />
+        {image && (
+          <>
+              <Annotation
+                src={image}
+                alt='Uploaded Image'
+                annotations={annotations}
+                value={annotation}
+                onChange={onChange}
+                onSubmit={onSubmit}
+                allowTouch
+              />
+
+              <p>
+                x: {X}
+                <br/>
+                y: {Y}
+                <br/>
+                height: {H}
+                <br/>
+                width: {W}
+              </p>
+          </>
+        )}
+        
        
       </header>
     </div>
